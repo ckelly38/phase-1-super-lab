603150e7315e27035b99226df46bb859e043996a
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-07-12 03:49:13 -0600

    using super method and object example done

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your classes here
+//NOT THE DATA STRUCTURE
+class Tree{
+    constructor(tp)
+    {
+        this.species = tp;
+    }
+
+    static definition()
+    {
+        return "A tree is a perennial plant with an elongated stem, or trunk, " +
+            "supporting branches and leaves.";
+    }
+}
+
+class Deciduous extends Tree{
+    constructor(tp, nm)
+    {
+        super(tp);
+        this.name = nm;
+    }
+
+    static definition(){
+        return super.definition() + " Deciduous trees shed their leaves annually.";
+    }
+}
+
+class Evergreen extends Tree{
+    constructor(tp, nm)
+    {
+        super(tp);
+        this.name = nm;
+    }
+
+    static definition(){
+        return super.definition() + " Evergreens keep their leaves all year round.";
+    }
+}
